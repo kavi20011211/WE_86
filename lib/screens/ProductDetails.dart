241fd0760775abca569369e96fabe06bb6997a8f
Uvindu Kavishka <kavishkauvindu0@gmail.com>
2024-05-09 12:15:48 +0530

    Completed

--- lib/screens/ProductDetails.dart
+++ lib/screens/ProductDetails.dart
@@ -49,74 +49,115 @@ class _ProductDetailsState extends State<ProductDetails> {
   Widget build(BuildContext context) {
     final dynamic data = ModalRoute.of(context)!.settings.arguments;
     return Scaffold(
-
-        appBar: AppBar(
-          backgroundColor: const Color(0xFF164650),
-          actions: [IconButton(onPressed: (){}, icon:const Icon(Icons.hearing_rounded))],
-        ),
-        body: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-
-          child: StreamBuilder<DocumentSnapshot>(
-            stream: _products.doc(data).snapshots(),
-            builder: (context,AsyncSnapshot<DocumentSnapshot>documentSnapshot){
-              if(documentSnapshot.hasData){
-                final DocumentSnapshot<Object?>?data = documentSnapshot.data;
-                return Card(
-                  margin:const EdgeInsets.only(left: 10,right: 10,top: 20, bottom: 20),
-                  child: Container(
-
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                       ClipRRect(
-                        borderRadius:const BorderRadius.only(topLeft: Radius.circular(16.0),topRight: Radius.circular(16.0)),
-                        child: Image.network(data?["image"],height: 400,fit: BoxFit.cover,width: double.infinity,
-                        errorBuilder: (context, error, stackTrace) {
-                          return const Text('Failed to load image');
-                        },)
-                       ),
-
-                       Padding(padding: const EdgeInsets.all(8.0),
-                       child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(data?["ProductTitle"],style:const TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-                          const SizedBox(height: 8.0,),
-                          Text(data?["ProductDescription"],),
-                          const SizedBox(height: 8.0,),
-                          Row(children: [
-                            const Text("Sizes: ",style: TextStyle(fontWeight: FontWeight.w700),),
-                            for(String i in data?["size"])
-                             Padding(padding:const EdgeInsets.only(left: 3),
-                            child: Text(" $i |"),)
-                          ],),
-                          const SizedBox(height: 8.0,),
-                          Row(children: [
-                            const Text("Colours: ",style: TextStyle(fontWeight: FontWeight.w700),),
-                            for(String i in data?["color"])
-                             Padding(padding:const EdgeInsets.only(left: 3),
-                            child: Text(" $i |"),)
-                          ],),
-                          const SizedBox(height: 8.0,),
-                          Row(children: [
-                            const Text("Price: ",style: TextStyle(fontWeight: FontWeight.w700),),
-                             Padding(padding:const EdgeInsets.only(left: 3),
-                            child: Text(data?["price"].toString()??""),)
-                          ],),
-
-                          
-                          Row(
-                            children: [
-                              IconButton(onPressed: ()async{await _addToCartList(data?["ProductTitle"], data?["price"]);}, icon:const Icon(Icons.add_shopping_cart_rounded,))
-                            ],
-                          )
-                        ],
-                       ),)
-                       
-                      ],
-                    ),
+      appBar: AppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: StreamBuilder<DocumentSnapshot>(
+          stream: _products.doc(data).snapshots(),
+          builder: (context, AsyncSnapshot<DocumentSnapshot> documentSnapshot) {
+            if (documentSnapshot.hasData) {
+              final DocumentSnapshot<Object?>? data = documentSnapshot.data;
+              return Card(
+                margin: const EdgeInsets.only(
+                    left: 10, right: 10, top: 20, bottom: 20),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(16.0),
+                              topRight: Radius.circular(16.0)),
+                          child: Image.network(
+                            data?["image"],
+                            height: 200,
+                            fit: BoxFit.cover,
+                            width: double.infinity,
+                            errorBuilder: (context, error, stackTrace) {
+                              return const Text('Failed to load image');
+                            },
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data?["ProductTitle"],
+                              style: const TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(
+                              height: 8.0,
+                            ),
+                            Text(
+                              data?["ProductDescription"],
+                            ),
+                            const SizedBox(
+                              height: 8.0,
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  "Sizes: ",
+                                  style: TextStyle(fontWeight: FontWeight.w700),
+                                ),
+                                for (String i in data?["size"])
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 3),
+                                    child: Text(" $i |"),
+                                  )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 8.0,
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  "Colours: ",
+                                  style: TextStyle(fontWeight: FontWeight.w700),
+                                ),
+                                for (String i in data?["color"])
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 3),
+                                    child: Text(" $i |"),
+                                  )
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 8.0,
+                            ),
+                            Row(
+                              children: [
+                                const Text(
+                                  "Price: ",
+                                  style: TextStyle(fontWeight: FontWeight.w700),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 3),
+                                  child: Text(data?["price"].toString() ?? ""),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                    onPressed: () async {
+                                      await _addToCartList(
+                                          data?["ProductTitle"],
+                                          data?["price"]);
+                                    },
+                                    icon: const Icon(
+                                      Icons.add_shopping_cart_rounded,
+                                    ))
+                              ],
+                            )
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
               );
